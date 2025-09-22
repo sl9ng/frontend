89339b7d499a4b577fe79e53d59822d62236af57
sl9ng <joazin.gyn01@gmail.com>
2025-09-22 11:18:46 -0300

    url fix 4

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://frontend-aj1u.onrender.com",
+    baseURL: "https://uaieats-backend-3y2c.onrender.com",
 });
 
 api.interceptors.request.use(
